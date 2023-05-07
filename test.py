4a850be497b8eca03b79a269c91b01b20a072737
unknown <roshanpimpalkar1@gmail.com>
2023-05-07 16:18:34 +0530

    config file ready to launch

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from main import to_upper
 
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
-        name = "roshan"
+        name = "Shubham"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "ROSHAN")
+        self.assertEqual(upper_name, "SHUBHAM")
 
 
 if __name__ == '__main__':
